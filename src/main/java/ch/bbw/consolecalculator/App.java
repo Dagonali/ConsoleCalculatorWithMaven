9772f11bfbc87f6bbe747942710cd0928bfb0568
Dagonali <ali.spielt01@gmail.com>
2020-01-17 14:35:05 +0100

    Initialized summe

--- src/main/java/ch/bbw/consolecalculator/App.java
+++ src/main/java/ch/bbw/consolecalculator/App.java
@@ -1,13 +1,23 @@
 package ch.bbw.consolecalculator;
 
+import java.sql.SQLOutput;
+
 /**
- * Hello world!
+ * @author Ali Da Silva Ouederni
+ * @version Jan. 17
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+        Calculator calculator = new Calculator();
+        int valueA = 10;
+        int valueB = 20;
+
+        System.out.println("Conosle Calculator");
+        System.out.println("==================\n");
+
+        System.out.println("Summe " + valueA + " + " + valueB + " = " + calculator.summe(valueA, valueB));
     }
 }
